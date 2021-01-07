e5ea8c778a583134821eddb8aa58f3d9e6cae5d6
Jacek Starz <jacek.starz@gmail.com>
2021-01-07 20:51:09 +0100

    💥 Add validation to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,16 @@
 class Ticket < ApplicationRecord
+  validates :name,
+    :presence => true,
+    :length => { minimum: 5 }
+
+  validates :address,
+    :presence => true
+
+  validates :email_address,
+    :presence => true,
+    :format => { with: URI::MailTo::EMAIL_REGEXP }
+  
+  validates :price,
+    :presence => true,
+    :numericality => { greater_than: 0 }
 end
