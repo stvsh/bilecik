680acf751af22fe52c88275ec3fe49b48639735b
Jacek Starz <jacek.starz@gmail.com>
2021-01-27 03:59:44 +0100

    🎨 Visual changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events, :only => [:index, :new, :create, :show]
   resources :tickets
-  root :to => "application#index"
+  root :to => "welcome#index"
 end
